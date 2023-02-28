f311910fec6307ae383db781dd41da79b22f36fd
Mustapha Bouh <67738751+mostapha@users.noreply.github.com>
2023-02-28 22:51:13 +0100

    4.4

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -3,6 +3,10 @@ const dummy = (blogs) => {
   return 1
 }
 
+const totalLikes = (blogs) => {
+  return blogs.reduce((pv, v) => pv + v.likes, 0)
+}
+
 module.exports = {
-  dummy
+  dummy, totalLikes
 }
