2352e90319fe7f49041d41ee8edc3f4093c3ae63
Mustapha Bouh <67738751+mostapha@users.noreply.github.com>
2023-02-28 23:16:45 +0100

    4.5*

--- tests/list_helper.test.js
+++ tests/list_helper.test.js
@@ -1,5 +1,32 @@
 const listHelper = require('../utils/list_helper')
 
+const blogList = [
+  {
+    _id: '5a422aa71b54a676234d17f8',
+    title: 'Go To Statement Considered Harmful',
+    author: 'Edsger W. Dijkstra',
+    url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+    likes: 5,
+    __v: 0
+  },
+  {
+    _id: '5a422b3a1b54a676234d17f9',
+    title: 'Canonical string reduction',
+    author: 'Edsger W. Dijkstra',
+    url: 'http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html',
+    likes: 10,
+    __v: 0
+  },
+  {
+    _id: '5a422ba71b54a676234d17fb',
+    title: 'TDD harms architecture',
+    author: 'Robert C. Martin',
+    url: 'http://blog.cleancoder.com/uncle-bob/2017/03/03/TDD-Harms-Architecture.html',
+    likes: 15,
+    __v: 0
+  },
+]
+
 test('dummy returns one', () => {
   const blogs = []
 
@@ -9,56 +36,33 @@ test('dummy returns one', () => {
 
 describe('total likes', () => {
 
-  // eslint-disable-next-line no-unused-vars
-  const oneBlogList = [{
-    _id: '5a422a851b54a676234d17f7',
-    title: 'React patterns',
-    author: 'Michael Chan',
-    url: 'https://reactpatterns.com/',
-    likes: 5,
-    __v: 0
-  }]
-
-  const blogList = [
-    {
-      _id: '5a422aa71b54a676234d17f8',
-      title: 'Go To Statement Considered Harmful',
-      author: 'Edsger W. Dijkstra',
-      url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
-      likes: 5,
-      __v: 0
-    },
-    {
-      _id: '5a422b3a1b54a676234d17f9',
-      title: 'Canonical string reduction',
-      author: 'Edsger W. Dijkstra',
-      url: 'http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html',
-      likes: 10,
-      __v: 0
-    },
-    {
-      _id: '5a422ba71b54a676234d17fb',
-      title: 'TDD harms architecture',
-      author: 'Robert C. Martin',
-      url: 'http://blog.cleancoder.com/uncle-bob/2017/03/03/TDD-Harms-Architecture.html',
-      likes: 15,
-      __v: 0
-    },
-  ]
-
   test('empty list equals zero', () => {
     const result = listHelper.totalLikes([])
     expect(result).toBe(0)
   })
 
   test('one blog list returns likes of the blog', () => {
-    const result = listHelper.totalLikes(oneBlogList)
-    expect(result).toBe(oneBlogList[0].likes)
+    const result = listHelper.totalLikes(blogList.slice(0, 1))
+    expect(result).toBe(blogList[0].likes)
   })
 
   test('accuratly calculates filled blog list', () => {
     const result = listHelper.totalLikes(blogList)
     expect(result).toBe(30)
+  })
+
+})
+
+describe('favorite blog', () => {
+
+  test('finds blog with the most likes', () => {
+    const result = listHelper.favoriteBlog(blogList)
+    console.log('result', result)
+    expect(result).toEqual({
+      title: 'TDD harms architecture',
+      author: 'Robert C. Martin',
+      likes: 15
+    })
   })
 
 })
--- utils/list_helper.js
+++ utils/list_helper.js
@@ -7,6 +7,18 @@ const totalLikes = (blogs) => {
   return blogs.reduce((pv, v) => pv + v.likes, 0)
 }
 
+const favoriteBlog  = blogs => {
+
+  const mostLikes = Math.max(...blogs.map(e => e.likes))
+  const blogWithMostLikes = blogs.find(b => b.likes === mostLikes)
+
+  return {
+    title: blogWithMostLikes.title,
+    author: blogWithMostLikes.author,
+    likes: blogWithMostLikes.likes
+  }
+}
+
 module.exports = {
-  dummy, totalLikes
+  dummy, totalLikes, favoriteBlog
 }
