450f68b1cc82f317d0df19391f20c77a954ec374
Mustapha Bouh <67738751+mostapha@users.noreply.github.com>
2023-02-28 16:15:28 +0100

    4.2

---
+++ app.js
@@ -0,0 +1,21 @@
+const config = require('./utils/config')
+const mongoose = require('mongoose')
+const logger = require('./utils/logger')
+const express = require('express')
+const app = express()
+const cors = require('cors')
+const blogsRouter = require('./controllers/blogs')
+
+
+const mongoUrl = config.MONGODB_URI
+mongoose.connect(mongoUrl).then(() => {
+  logger.info('connected to MongoDB')
+}).catch((error) => {
+  logger.error('error connecting to MongoDB:', error.message)
+})
+
+app.use(cors())
+app.use(express.json())
+app.use('/api/blogs', blogsRouter)
+
+module.exports = app
