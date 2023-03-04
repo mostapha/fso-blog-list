dddaf638c90019ea40881f50387cf876224771eb
Mustapha Bouh <67738751+mostapha@users.noreply.github.com>
2023-03-04 23:37:50 +0100

    4.15

--- app.js
+++ app.js
@@ -5,7 +5,9 @@ const express = require('express')
 require('express-async-errors')
 const app = express()
 const cors = require('cors')
+
 const blogsRouter = require('./controllers/blogs')
+const usersRouter = require('./controllers/users')
 
 
 const mongoUrl = config.MONGODB_URI
@@ -17,6 +19,10 @@ mongoose.connect(mongoUrl).then(() => {
 
 app.use(cors())
 app.use(express.json())
+
 app.use('/api/blogs', blogsRouter)
+app.use('/api/users', usersRouter)
+
+
 
 module.exports = app
