2892a0676713c195b912664fa4a05bc04bab9abb
Mustapha Bouh <67738751+mostapha@users.noreply.github.com>
2023-03-05 16:56:20 +0100

    4.18 and 4.19

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ require('express-async-errors')
 const app = express()
 const cors = require('cors')
 const middleware = require('./utils/middleware')
+const loginRouter = require('./controllers/login')
+
 
 mongoose.set('strictQuery', false)
 
@@ -23,6 +25,7 @@ mongoose.connect(mongoUrl).then(() => {
 app.use(cors())
 app.use(express.json())
 
+app.use('/api/login', loginRouter)
 app.use('/api/blogs', blogsRouter)
 app.use('/api/users', usersRouter)
 
--- controllers/blogs.js
+++ controllers/blogs.js
@@ -1,11 +1,21 @@
 const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
 const User = require('../models/user')
+const jwt = require('jsonwebtoken')
+
 
 blogsRouter.get('/', async (request, response) => {
   const blogs = await Blog.find({}).populate('user')
   response.json(blogs)
 })
+
+const getTokenFrom = request => {
+  const authorization = request.get('authorization')
+  if (authorization && authorization.startsWith('Bearer ')) {
+    return authorization.replace('Bearer ', '')
+  }
+  return null
+}
 
 blogsRouter.post('/', async (request, response) => {
 
@@ -23,7 +33,11 @@ blogsRouter.post('/', async (request, response) => {
     })
   }
 
-  const user = await User.findOne({})
+  const decodedToken = jwt.verify(getTokenFrom(request), process.env.SECRET)
+  if (!decodedToken.id) {
+    return response.status(401).json({ error: 'token invalid' })
+  }
+  const user = await User.findById(decodedToken.id)
 
   const blog = new Blog({
     title,
